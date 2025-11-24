7bd38519f54c73c4a3eeb8cab030bfca3c779e6a
Sergii Bondariev <s.bondariev@bossgs.com>
2025-11-24 14:31:44 +0200

    Removed unused code

--- src/app/constant/base-game-settings.const.ts
+++
@@ -1,8 +0,0 @@
-import { GameSettings } from '../models/game.model';
-
-export const BASE_GAME_SETTINGS: GameSettings = {
-    fallingSpeed: 6, // px per second
-    fallingFrequency: 800, // ms
-    playerSpeed: 12, // px per key press
-    gameTime: 20 // seconds
-};
